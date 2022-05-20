832934249ab1dff7aa83aef7948b48246604ba28
Abdulrahman Mohammed <abdulrahman48811@gmail.com>
2022-05-20 02:39:38 -0400

    completed loading component starting the logout page/sidebar

--- messagez/components/Loading.js
+++ messagez/components/Loading.js
@@ -1,6 +1,9 @@
+import { Circle } from "better-react-spinkit";
+
+
 function Loading() {
   return (
-    <center>
+    <center style={{ display: 'grid', placeItems: 'center', height: '100vh' }}>
       <div>
         <img
           src='https://lh3.googleusercontent.com/V3lOQY-1L-0T0U6bo4wiojKZkRMKe8_Ru0yO70he1Hc0dWD17Tb2nYbHGMpHtxhzyVhFv8MiQRmjE6LdpP1Hn5_Gze2GzQ7PZhCwmc5bCBpfa3dwwoelPlectBQsurvorODc_p3gIg=w2400'
@@ -8,6 +11,7 @@ function Loading() {
           style={{marginBottom:10}}
           height={100}
         />
+        <Circle color="#CF9FFF" size={60}/>
       </div>
       </center>
   )
